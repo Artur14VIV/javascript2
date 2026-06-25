fb5f69e4b11aa4d89a6629892c1a06d4199f1978
Artur14VIV <artur.fritzen@escola.pr.gov.br>
2026-06-25 11:40:33 -0300

    Log client information and CPF digits

--- acesso_ponto.js
+++ acesso_ponto.js
@@ -4,3 +4,9 @@ const cliente = {
   cpf: "1122233345",
   email: "andre@dominio.com",
 };
+
+console.log(
+  `O nome do cliente é ${cliente.nome} e essa pessoa tem ${cliente.idade} anos.`
+);
+
+console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
